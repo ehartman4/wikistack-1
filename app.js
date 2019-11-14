c8102c8aa4cfd0ac98540e1692bce8e2c8b38796
Ariana Hwang <ajhwang0717@gmail.com>
2019-11-14 11:28:31 -0500

    added package.json, middlewares, app.js file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: false}));
+
+
+app.get('/', (req, res) => {
+    console.log('hello world');
+})
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+    console.log(`App listening to port ${PORT}`);
+})
